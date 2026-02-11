4c1fcce5e7672904bc22f62dc29b0dcd84fbfc5d
paulom-s <paulsans@orange.fr>
2026-02-11 15:00:36 +0100

    Cleaning

--- src/com_apn.py
+++ src/com_apn.py
@@ -1,6 +1,10 @@
+<<<<<<< Updated upstream
 from requests import *
 
 url = 'http://localhost:5513/?slc='
+=======
+
+>>>>>>> Stashed changes
 
 def capture_LV():
     print('Downloading Live View...')
